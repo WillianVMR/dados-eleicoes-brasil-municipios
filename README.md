ba50e8e061726202bc928c44b924de0cde93f54c
Willian Ribeiro <57929088+WillianVMR@users.noreply.github.com>
2024-02-07 15:04:51 -0300

    Database criada em SQLITE e script para geoprocessamento criado

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dados-eleicoes-brasil-municipios
 Esse repositório tem a proposta de criar uma visualização geoespacial dos dados de eleições municipais brasileiras
+
+### Utilizando bibliotecas:
+- Pandas
+- Fiona
+- Shapely
+- Pyproj
+- Rtree
+- Geopandas
---
+++ processing_geolocalizacao_data.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import sqlite3
+import threading
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+
+# Connect to the SQLite database
+conn = sqlite3.connect('geolocalizacao_sp.sqlite3')
+cursor = conn.cursor()
+
+# Initialize the geocoder with rate limiter to respect usage limits
+geolocator = Nominatim(user_agent="geoapiExercises")
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+
+# Function to geocode address
+def geocode_address(address):
+    try:
+        location = geocode(address)
+        if location:
+            return location.latitude, location.longitude
+    except:
+        return None, None
+    return None, None
+
+# Flag to control the updating process
+stop_requested = False
+
+def check_for_stop():
+    global stop_requested
+    while not stop_requested:
+        user_input = input()
+        if user_input == 'stop':
+            stop_requested = True
+            print("Stop requested. Finishing up...")
+
+# Start the stop checking in a separate thread
+stop_thread = threading.Thread(target=check_for_stop)
+stop_thread.start()
+
+# Function to update rows
+def update_rows():
+    rows_updated = 0
+    total_rows = cursor.execute("SELECT COUNT(*) FROM dataset_capital WHERE GEOCODED = 0").fetchone()[0]
+    if total_rows == 0:
+        print("All rows have already been geocoded.")
+        return
+
+    for row in cursor.execute("SELECT rowid, FORMATADO_COM_DS_ENDERECO FROM dataset_capital WHERE GEOCODED = 0").fetchall():
+        if stop_requested:
+            break
+        rowid, address = row
+        latitude, longitude = geocode_address(address)
+        if latitude and longitude:
+            cursor.execute("UPDATE dataset_capital SET NEW_NR_LATITUDE = ?, NEW_NR_LONGITUDE = ?, GEOCODED = 1 WHERE rowid = ?", (latitude, longitude, rowid))
+            conn.commit()
+            rows_updated += 1
+            print(f"Updated row {rowid}. {rows_updated}/{total_rows} ({(rows_updated/total_rows)*100:.2f}%)")
+    
+    print(f"Total rows updated: {rows_updated}/{total_rows} ({(rows_updated/total_rows)*100:.2f}%)")
+
+# Call the update function
+update_rows()
+
+# Close the database connection
+conn.close()
+
+# Wait for the stop thread to finish
+stop_thread.join()
